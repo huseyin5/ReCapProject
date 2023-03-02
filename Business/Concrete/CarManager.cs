00d1f3210e16fd5bb8e649bbab856b41218ce1e9
huseyin5 <395273@ogr.ktu.edu.tr>
2023-03-02 14:14:40 +0300

    added to car services and manager system

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,20 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete;
+
+public class CarManager : ICarService
+{
+    private ICarDal _iCarDal;
+
+    public CarManager(ICarDal iCarDal)
+    {
+        _iCarDal = iCarDal;
+    }
+
+    public List<Car> GetAll()
+    {
+        return _iCarDal.GetAll();
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
 
-Console.WriteLine("Hello, World!");
+CarManager carManager = new CarManager(new InMemoryCarDal());
+
+foreach (var car in carManager.GetAll())
+{
+    Console.WriteLine(car.Id);
+}
